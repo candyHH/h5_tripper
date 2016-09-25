83720a48ac08930cb8f2cf6f1dd4f4fdc4f14b67
candyhh <17764576286@163.com>
2016-09-25 10:25:44 +0800

    部分数据库交互

---
+++ majia.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+
+var userInfo={
+  nickname:['奥观海','贝爷','陈冠希','陈学冬','福原爱','傅园慧','井柏然','金三胖','林更新','林志玲','刘诗诗','李易峰','鹿晗','罗志祥','普大帝','吴彦祖','吴亦凡','习大大','杨幂','杨洋','张翰','张继科','张局座','张艺新','赵丽颖','郑爽','周杰伦'],
+  img:['aoguanhai.jpg','beiye.jpg','chenguanxi.jpg','chenxuedong.jpg','fuyuanai.jpg','fuyuanhui.jpg','jinboran.jpg','jinsanpan.jpg','lingengxin.jpg','linzhilin.jpg','liushishi.jpg','liyifeng.jpg','luhan.jpg','luozhixiang.jpg','pudadi.jpg','wuyanzu.jpg','wuyifan.jpg','xidada.jpg','yangmi.kpg','yangyang.jpg','zhanghan.jpg','zhangjike.jpg','zhangjuzuo.jpg','zhangyixing.jpg','zhaoliyin.png','zhengshuang.jpg','zhoujielun.jpg'],
+  answer:['2,2,3,2,3,2,2,2,2,2','1,2,3,3,2,2,1,3,1,3','2,2,3,2,3,1,3,1,1,2','2,1,2,2,1,2,2,2,2,1','3,2,3,3,1,2,2,1,3,2','3,1,1,2,3,1,2,3,1,2','2,1,2,2,1,2,2,2,2,1','1,2,2,1,2,1,1,1,1,2','3,1,2,3,1,3,1,3,1,1','1,2,1,2,1,1,2,3,1,2','2,1,3,1,1,1,1,2,3,3','1,2,1,3,2,3,2,2,2,2','3,2,2,2,2,3,1,2,1,2','2,1,1,3,3,2,2,3,2,2','2,2,2,2,2,2,2,3,2,3','2,2,2,1,2,2,2,2,2,3','2,3,1,1,2,2,2,2,3,1','2,2,2,1,2,3,1,2,2,3','2,1,3,1,1,1,1,2,3,3','2,3,2,3,2,2,2,2,2,2','1,1,2,2,2,2,2,1,3,2','1,2,2,1,3,3,3,3,2,2','3,2,1,2,2,1,1,3,1,2','2,2,3,1,2,3,3,2,2,3','3,3,2,1,2,2,2,2,1,3','1,2,2,1,2,2,2,2,2,3','3,2,2,2,2,3,2,3,2,2']
+}
+
+
+module.exports = userInfo;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,143 @@
 var express = require('express');
 var router = express.Router();
+var superagent = require('superagent');
+var redis = require('redis');
+var config = require('../config.js');
+var userInfo = require('../majia.js');
+console.log(userInfo.nickname[0]);
+var client  = redis.createClient(config.redis.port,'127.0.0.1');
+// client.auth(config.redis.pwd);
+client.select('4');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var thisUrl = req.url;
+  var shareUrl = encodeURIComponent((global.browserURL + thisUrl).split('#')[0]);
+  console.log('shareUrl.................'+(global.browserURL + thisUrl).split('#')[0]);
+  var isPhone = false;
+  var agentID = req.headers['user-agent'].toLowerCase().search(/(iphone|ipod|ipad|android)/);
+  if (agentID) {
+      isPhone = true;
+  } else {
+      isPhone = false;
+  }
+  var openid = req.query.openid || '';
+  var access_token = req.query.access_token || '';
+  superagent
+      .get('https://api.weixin.qq.com/sns/userinfo?access_token=' + access_token + '&openid=' + openid + '&lang=zh_CN')
+      .end(function(err, res4) {
+          if (res4.text.indexOf('errcode') > 0 && isPhone) {
+              var state = encodeURIComponent((req.url).split('&openid')[0]);
+              // var state = encodeURIComponent('/pay/pay?id=960'.split('&openid')[0]);
+              console.log(state);
+              console.log(global.wechatURL + '/wechat_oauth/getAuthorizeURL?state=' + state+'&finalbase='+global.browserURL);
+              superagent
+                  .get(global.wechatURL + '/wechat_oauth/getAuthorizeURL?state=' + state+'&finalbase='+global.browserURL)
+                  .end(function(err, res3) {
+                      if (res3 !== undefined && res3.ok) {
+                          res.redirect(res3.text);
+                          return;
+                      } else {
+                          console.error('微信授权错误。');
+                          logger.error('微信授权错误。');
+                          res.render('error', {});
+                      }
+                  });
+          } else {
+              console.log(' 正常请求---------- ');
+              var info = JSON.stringify(res4);
+              var userInfo = JSON.parse(res4.text);
+
+              // 判断openid是否存在集合
+              var openid = userInfo.openid;
+              superagent
+                .get(global.wechatURL + '/wechat_api/jsconfig?url=' + shareUrl)
+                .end(function(err2, res2) {
+                  if (res2 !== undefined && res2.ok) {
+                    res2.body.browserUrl = global.browserURL;
+                    res2.body.nickname = userInfo.nickname;
+                    res.body.openid = userInfo.openid;
+                    res.body.img = userInfo.headimgurl;
+                    var string2= JSON.stringify(res2.body);
+                    console.log('分享成功啦！'+string2);
+                    res.render('welcome',res2.body);
+                  } else {
+                    console.error('微信分享api错误。');
+                  }
+                });
+          }
+      });
 });
+
+router.get('/index', function(req, res, next) {
+  var id = req.query.id;
+  //微信授权
+  if(id){
+    client.hget('tripperuser',id,function (err,result) {
+      if(err){
+        console.log(err);
+      }else{
+        result.id = id;
+        console.log('result...'+result);
+        res.render('index',JSON.parse(result));
+      }
+    })
+  }else{
+    var num = Math.floor(Math.random()*27+1);
+    console.log(num);
+    client.hget('tripperuser',num,function (err,result) {
+      if(err){
+        console.log(err);
+      }else{
+        result.id = num;
+        console.log('result...'+result);
+        var info = JSON.parse(result);
+        res.render('index',{result:info});
+      }
+    })
+  }
+});
+
+router.get('/result',function (req,res,next) {
+  var num = Math.floor(Math.random()*27+1);
+  res.render('index', { title: 'Express' });
+})
+
+router.get('/addData', function(req, res, next) {
+  // for(var i =0;i<27;i++){
+  //   client.incr('uid');
+  //   client.get('uid',function (err,uid) {
+  //     var num = uid-1;
+  //     client.hmset('tripperuser',uid,JSON.stringify({
+  //       name:userInfo.nickname[num],
+  //       img:global.browserURL+'/images/'+userInfo.img[num],
+  //       answer:userInfo.answer[num],
+  //       match:''
+  //     }))
+  //   })
+  // }
+  // client.hset('tripperuserId','name','id');
+  client.hget('tripperuserId','userid',function (err,flag) {
+    if(flag == null || flag == ''){
+      console.log('不存在');
+    }else{
+      console.log('存在');
+      console.log(flag);
+    }
+    res.render('addData', { title: 'Express' });
+  });
+});
+
+router.post('/post',function (req,res,next) {
+  var userInfo = {
+    name:req.body.name,
+    img:req.body.img,
+    answer:req.body.answer
+  }
+  client.incr('uid');
+  client.get('uid',function (err,uid) {
+    client.hset('tripperuser',uid,JSON.stringify(userInfo));
+  })
+})
 
 module.exports = router;
