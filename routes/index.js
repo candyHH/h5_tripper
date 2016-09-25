7751d557dbdd9e4c6a8b9559fd9a4d96be022303
su zhao <suzhao@sumatoMacBook-Air.local>
2016-09-25 13:13:27 +0800

    new

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '十一出行先看看旅伴靠谱啵！' });
+});
+
+router.get('/result', function(req, res, next) {
+  res.render('result', { title: '十一出行先看看旅伴靠谱啵！' });
 });
 
 module.exports = router;
