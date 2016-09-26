d677259fdf2a20d80013f8f6cc3e106d5b804484
su zhao <suzhao@sumatoMacBook-Air.local>
2016-09-26 18:56:33 +0800

    ll

--- public/javascripts/result.js
+++ public/javascripts/result.js
@@ -1,56 +1,54 @@
 
-// var selfInfo = sessionStorage.getItem('selfInfo');
-// var shareInfo = sessionStorage.getItem('shareInfo');
-// alert(selfInfo);
-// alert(shareInfo);
+
+
 
   $('.share-text-name').html(shareInfo.name);
-    $('.potrait-img').attr('src',shareInfo.img);
+  $('.potrait-img').attr('src',shareInfo.img);
 
 
 
 var vmodel;
 avalon.ready(function(){
 	vmodel = avalon.define('info', function(vm){
-		vm.shareInfo = shareInfo;
-		vm.vestInfo = vestInfo;
-		// vm.match = '';
+		vm.shareInfo = {
+			name : shareInfo.name
+			,img : shareInfo.img
+			,answer : shareInfo.answer
+			,id : shareInfo.id
+			,match : 0
+		};
+		// vm.vestInfo = {};
 	});
 })
+vmodel.shareInfo
 
 // alert(selfInfo.answer);
 // 匹配答案
-var selfAnser = selfInfo.answer.split('-');
-var shareAnser = shareInfo.answer.split('-');
-for (var i = 0; i < vestInfo.length; i++) {
-	vestInfo[i].answer = vestInfo[i].answer.split(',');
-};
-
 // 与分享者匹配
 var flag = 0;
 for (var i = 0; i < 10; i++) {
-	if (selfAnser[i] == shareAnser[i]) {
-		flag++;
-	};
+    if (selfAnser[i] == shareAnser[i]) {
+        flag++;
+    };
 };
 
 vmodel.shareInfo.match = flag;
 
 // 与10个马甲匹配
 // var num = 0;
-for (var j = 0; j < vestInfo.length; j++) {
-	var flag = 0;
-	for (var i = 0; i < 10; i++) {
-		if (selfAnser[i] == vestInfo[j].answer[i]) {
-			flag++;
-		};
-	};
-	vestInfo[j].match = flag;
-};
+// for (var j = 0; j < vestInfo.length; j++) {
+// 	var flag = 0;
+// 	for (var i = 0; i < 10; i++) {
+// 		if (selfAnser[i] == vestInfo[j].answer[i]) {
+// 			flag++;
+// 		};
+// 	};
+// 	vestInfo[j].match = flag;
+// };
 
- vmodel.vestInfo = vestInfo.sort(function(a,b){
- 	return b.match - a.match;
- })
+//  vmodel.vestInfo = vestInfo.sort(function(a,b){
+//  	return b.match - a.match;
+//  })
 
 
 
