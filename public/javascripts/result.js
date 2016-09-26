765c166bfac02b35dbd5227f1ab04ca7c0b06dc0
candyhh <17764576286@163.com>
2016-09-26 19:56:05 +0800

    ghhhh

--- public/javascripts/result.js
+++ public/javascripts/result.js
@@ -1,41 +1,56 @@
-var vmodel;
-avalon.ready(function(){
-	vmodel = avalon.define('info', function(vm){
-		vm.shareInfo = shareInfo;
-		vm.vestInfo = vestInfo;
-	});
+// var vmodel;
+// avalon.ready(function(){
+// 	vmodel = avalon.define({
+// 		 $id: "info",
+// 		 shareInfo: shareInfo,
+// 		 vestInfo: vestInfo
+// 	})
+// 	avalon.scan()
+//   console.log(vmodel.shareInfo);
+//
+// 	// 匹配答案
+// 	// 与分享者匹配
+// 	var flag = 0;
+// 	for (var i = 0; i < 10; i++) {
+// 	    if (selfAnser[i] == shareAnser[i]) {
+// 	        flag++;
+// 	    };
+// 	};
+//
+// 	vmodel.shareInfo.match = flag;
+//
+// 	// 与10个马甲匹配
+// 	var num = 0;
+// 	for (var j = 0; j < vestInfo.length; j++) {
+// 		var flag = 0;
+// 		for (var i = 0; i < 10; i++) {
+// 			if (selfAnser[i] == vestInfo[j].answer[i]) {
+// 				flag++;
+// 			};
+// 		};
+// 		vestInfo[j].match = flag;
+// 	};
+//
+//
+// 	 vmodel.vestInfo = vestInfo.sort(function(a,b){
+// 	 	return b.match - a.match;
+// 	 })
+//
+//
+//
+// })
 
-	// 匹配答案
-	// 与分享者匹配
-	var flag = 0;
-	for (var i = 0; i < 10; i++) {
-	    if (selfAnser[i] == shareAnser[i]) {
-	        flag++;
-	    };
-	};
-
-	vmodel.shareInfo.match = flag;
-
-	// 与10个马甲匹配
-	var num = 0;
-	for (var j = 0; j < vestInfo.length; j++) {
-		var flag = 0;
-		for (var i = 0; i < 10; i++) {
-			if (selfAnser[i] == vestInfo[j].answer[i]) {
-				flag++;
-			};
-		};
-		vestInfo[j].match = flag;
-	};
-
-
-	 vmodel.vestInfo = vestInfo.sort(function(a,b){
-	 	return b.match - a.match;
-	 })
-
-
-
+new Vue({
+  el: '#info',
+  data: {
+    message: 'Hello Vue.js!',
+		shareInfo: shareInfo,
+		vestInfo: vestInfo
+  }
 })
+
+
+
 
 // alert(selfInfo.answer);
 
