a50567d63d6396d319c98058bfc611d857558c61
candyhh <17764576286@163.com>
2016-09-30 09:46:54 +0800

    名单

--- public/javascripts/show.js
+++ public/javascripts/show.js
@@ -1,8 +1,10 @@
 var userInfo  = info.split('*');
+console.log(userInfo);
 for(var i = 0;i<userInfo.length;i++){
   userInfo[i] = JSON.parse(userInfo[i]);
 }
 
+console.log(userInfo);
 
 new Vue({
   el: '#show',
